d10eaab529d4995d13e3757ad1bf96832bf7e98b
jay3871 <janiszakis03@gmail.com>
2025-04-17 11:03:50 +0300

    Created models, relationships and migrations.

---
+++ app/Models/Book.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Book extends Model
+{
+    // Attiecība: grāmatai var būt vairāki autori
+    public function authors(): BelongsToMany
+    {
+        return $this->belongsToMany(Author::class);
+    }
+
+    // Attiecība: grāmatai var būt daudzi pirkumi
+    public function purchases(): HasMany
+    {
+        return $this->hasMany(Purchase::class);
+    }
+}
