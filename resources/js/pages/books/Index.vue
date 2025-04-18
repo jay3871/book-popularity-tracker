e744f92a6ea9745289a0931d0d6aed8e72ae113a
jay3871 <janiszakis03@gmail.com>
2025-04-18 17:35:00 +0300

    Added lang attribute to Index.vue file script tag.

--- resources/js/pages/books/Index.vue
+++ resources/js/pages/books/Index.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { router, Head } from '@inertiajs/vue3'
 import { ref, watch } from 'vue'
 import debounce from 'lodash/debounce' // Aiztures funkcija, lai nesūtītu pieprasījumus katru rakstīšanas reizi
